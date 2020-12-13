41c5f53726d1d97188b89d46674026e2d81c1427
Ted Brown <ted@oreganik.com>
2020-12-13 12:21:00 -0800

    object mover that looks more and more like itween every day :(

---
+++ Assets/Jambox/Code/Utilities/ObjectMoveData.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jambox
+{
+	/// <summary>
+	/// Used by ContentMover
+	/// </summary>
+	public class ObjectMoveData 
+	{
+		public Action OnComplete;
+
+		public bool IsComplete
+		{
+			get { return timer.IsComplete; }
+		}
+		
+		public Transform transform { get; private set; }
+		public Vector3 startPosition { get; private set; }
+		public Vector3 endPosition { get; private set; }
+		public Vector3 startScale { get; private set; }
+		public Vector3 endScale { get; private set; }
+		public Quaternion startRotation { get; private set; }
+		public Quaternion endRotation { get; private set; }
+		public bool rotate { get; private set; }
+		public bool scale { get; private set; }
+		public bool translate { get; private set; }
+		public Timer timer { get; private set; }
+		
+		private bool _rotateWorldSpace;
+		private bool _translateWorldSpace;
+
+		private const float HALF_PI = Mathf.PI / 2;
+
+		public ObjectMoveData (Transform targetTransform, float duration)
+		{
+			transform = targetTransform;
+			timer = new Timer(duration);
+		}
+
+		public void FinishNow ()
+		{
+			timer.FinishNow();
+			Process(1); // send a dummy time: the timer is finished so we'll hit max lerp no matter what
+
+			if (OnComplete != null)
+			{
+				OnComplete();
+			}
+
+			// Clear these functions so they won't be called twice
+			OnComplete = null;
+		}
+
+		public void Rotate (Quaternion start, Quaternion end, bool inWorldSpace = true)
+		{
+			startRotation = start;
+			endRotation = end;
+			_rotateWorldSpace = inWorldSpace;
+
+			if (inWorldSpace)
+			{
+				transform.rotation = start;
+			}
+			else
+			{
+				transform.localRotation = start;
+			}
+
+			rotate = true;
+		}
+
+		public void Scale (Vector3 start, Vector3 end)
+		{
+			startScale = start;
+			endScale = end;
+			transform.localScale = start;
+			scale = true;
+		}
+
+		public void Translate (Vector3 start, Vector3 end, bool inWorldSpace = true)
+		{
+			startPosition = start;
+			endPosition = end;
+			_translateWorldSpace = inWorldSpace;
+
+			if (inWorldSpace)
+			{
+				transform.position = start;
+			}
+			else
+			{
+				transform.localPosition = start;
+			}
+
+			translate = true;
+		}
+
+		public void Process (float deltaTime)
+		{
+			timer.Update(deltaTime);
+
+			// really rough approximation of a limit using a sin function
+			float t = Mathf.Sin(timer.t * HALF_PI);
+
+			if (rotate)
+			{
+				if (_rotateWorldSpace)
+				{
+					transform.rotation = Quaternion.Lerp(startRotation, endRotation, t);
+				}
+				else
+				{
+					transform.localRotation = Quaternion.Lerp(startRotation, endRotation, t);
+				}
+			}
+
+			if (scale)
+			{
+				transform.localScale = Vector3.Lerp(startScale, endScale, t);
+			}
+
+			if (translate)
+			{
+				if (_translateWorldSpace)
+				{
+					transform.position = Vector3.Lerp(startPosition, endPosition, t);
+				}
+				else
+				{
+					transform.localPosition = Vector3.Lerp(startPosition, endPosition, t);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Jambox/Code/Utilities/ObjectMover.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Jambox
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ObjectMover : MonoBehaviour 
+	{
+		private static List<GameObject> _objects;
+		private static Dictionary<GameObject, ObjectMoveData> _moveData;
+
+		public static void AddFinishAction (GameObject targetObject, Action action)
+		{
+			if (_moveData == null)
+			{
+				Debug.LogError("Is there not an instance of ContentMover in the scene?");
+			}
+		}
+
+		// todo: optionally reset position to start
+		public static void Cancel (GameObject targetObject)
+		{
+			if (_objects == null)
+			{
+				Debug.LogError("Is there not an instance of ContentMover in the scene?");
+			}
+
+			_moveData.Remove(targetObject);
+			_objects.Remove(targetObject);
+		}
+
+		public static ObjectMoveData Rotate (GameObject targetObject, Quaternion end, float duration, bool inWorldSpace = true)
+		{
+			return Rotate(targetObject, targetObject.transform.rotation, end, duration, inWorldSpace);
+		}
+
+		public static ObjectMoveData Rotate (GameObject targetObject, Quaternion start, Quaternion end, float duration, bool inWorldSpace = true)
+		{
+			ObjectMoveData moveData = GetMoveData(targetObject, duration);
+			moveData.Rotate(start, end, inWorldSpace);
+			return moveData;
+		}
+
+		public static ObjectMoveData Scale (GameObject targetObject, Vector3 end, float duration)
+		{
+			return Scale(targetObject, targetObject.transform.localScale, end, duration);
+		}
+
+		public static ObjectMoveData Scale (GameObject targetObject, Vector3 start, Vector3 end, float duration)
+		{
+			ObjectMoveData moveData = GetMoveData(targetObject, duration);
+			moveData.Scale(start, end);
+			return moveData;
+		}
+
+		public static ObjectMoveData Translate (GameObject targetObject, Vector3 end, float duration, bool inWorldSpace = true)
+		{
+			return Translate(targetObject, targetObject.transform.position, end, duration, inWorldSpace);
+		}
+
+		public static ObjectMoveData Translate (GameObject targetObject, Vector3 start, Vector3 end, float duration, bool inWorldSpace = true)
+		{
+			ObjectMoveData moveData = GetMoveData(targetObject, duration);
+			moveData.Translate(start, end, inWorldSpace);
+			return moveData;
+		}
+
+		private static ObjectMoveData GetMoveData (GameObject targetObject, float duration)
+		{
+			if (_moveData == null)
+			{
+				Debug.LogError("Is there not an instance of ContentMover in the scene?");
+			}
+
+			ObjectMoveData moveData = null;
+			if (_moveData.TryGetValue(targetObject, out moveData) == false)
+			{
+				moveData = new ObjectMoveData(targetObject.transform, duration);
+				_moveData.Add(targetObject, moveData);
+				_objects.Add(targetObject);
+			}
+			return moveData;
+		}
+
+		protected void Awake ()
+		{
+			_moveData = new Dictionary<GameObject, ObjectMoveData>();
+			_objects = new List<GameObject>();
+		}
+
+		protected void Update ()
+		{
+			int count = _objects.Count;
+			ObjectMoveData moveData = null;
+			float deltaTime = Time.deltaTime;
+
+			for (int i = count - 1; i >= 0; i--)
+			{
+				if (_moveData.TryGetValue(_objects[i], out moveData))
+				{
+					moveData.Process(deltaTime);
+					if (moveData.IsComplete)
+					{
+						moveData.FinishNow();
+						_moveData.Remove(_objects[i]);
+						_objects.RemoveAt(i);
+					}
+				}
+			}
+		}
+
+	}
+}
